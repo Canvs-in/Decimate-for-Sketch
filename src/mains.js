f75240108c48336e6b3d87293cdf106dfdf44011
Depro20 <roy@canvs.in>
2020-05-03 13:12:32 +0530

    separated font and shape cleaning

--- src/mains.js
+++ src/mains.js
@@ -5,14 +5,6 @@ var sg = sketch.getSelectedDocument().selectedLayers.layers[0].sketchObject;
 var sg_s=sketch.getSelectedDocument().selectedLayers.layers[0]
 
 
-// documentation: https://developer.sketchapp.com/reference/api/
-
-// export default function() {
-//   sketch.UI.message("It's alive 🙌")
-// }
-
-
-
 function roundoff(h){
       
   if ((h% 1) > 0.5) {var h = Math.ceil(h);} 
@@ -50,8 +42,56 @@ function iter(layer) {
 }
 
 
+function font_check(layer){
+  if (layer.layers) {
+    layer.layers.forEach(iter);
+  }
+
+  if (layer.type == "Text") { 
+    var fs=layer.style.fontSize
+      
+    layer.style.fontSize=roundoff(fs)}
+
+
+}
+
+
+function shape_check(layer){
+  if (layer.layers) {
+    layer.layers.forEach(iter);
+  }
+
+  if (layer.type == "ShapePath") { 
+    var w=layer.frame.width
+    var h=layer.frame.height  
+    layer.frame.width=roundoff(w)
+    layer.frame.height=roundoff(h)
+
+    var points=layer.points
+    if(points){
+    points.forEach(function(point){
+        var cr=point.cornerRadius
+
+        point.cornerRadius=roundoff(cr)
+    })}
+    }
+
+
+}
+
 export default function onRun(context){
 iter(selectedLayers.layers[0] )
 log("done")
 }
 
+export function font_rec(context){
+  font_check(selectedLayers.layers[0] )
+  log("done")
+
+}
+
+export function shape_rec(context){
+  shape_check(selectedLayers.layers[0] )
+  log("done")
+
+}
